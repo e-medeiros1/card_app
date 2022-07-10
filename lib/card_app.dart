9812adaf6ba86021e7fe915930afa7b1fba24760
Medeiros <hsymetry@gmail.com>
2022-07-09 21:35:03 -0300

    Implement url_launcher to all buttons

--- lib/card_app.dart
+++ lib/card_app.dart
@@ -1,6 +1,7 @@
 import 'package:card_app/widgets/containers.dart';
 import 'package:card_app/widgets/texts.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CardApp extends StatefulWidget {
   const CardApp({Key? key}) : super(key: key);
@@ -10,62 +11,58 @@ class CardApp extends StatefulWidget {
 }
 
 class _CardAppState extends State<CardApp> {
+  // ignore: unused_field
+  Future<void>? _launched;
+  //Github
+  final Uri toLaunch =
+      Uri(scheme: 'https', host: 'www.github.com', path: '/e-medeiros1/');
+  //Linkedin
+  final Uri toLaunchWeb =
+      Uri(scheme: 'https', host: 'www.linkedin.com', path: '/in/erimedeiros/');
+  //Gmail
+  final Uri emailLaunchUri = Uri(
+    scheme: 'mailto',
+    path: 'hsymetry@gmail.com',
+  );
+
+  final Uri? toWhatsapp = Uri(
+    scheme: 'https',
+    host: 'wa.me',
+    path: '559981173168',
+  );
+
+//Whatsapp
+
+  Future<void> _launchWhatsapp(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalNonBrowserApplication,
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+//Email
+  Future<void> _launchInMail(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.platformDefault,
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchInWeb(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    AlertDialog dialog = AlertDialog(
-      backgroundColor: Colors.blue.shade50,
-      title: const Center(
-        child: Text(
-          'Contatos',
-          style: TextStyle(
-              fontFamily: 'SourceSansPro',
-              fontWeight: FontWeight.w900,
-              letterSpacing: 2,
-              color: Colors.deepOrange),
-        ),
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ListTile(
-            leading: Image.network(
-              'https://cdn-icons-png.flaticon.com/512/25/25231.png',
-              scale: 0.5,
-              fit: BoxFit.cover,
-              color: Colors.deepOrange,
-            ),
-            title: const Text(
-              'github.com/e-medeiros1',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 18,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ListTile(
-            leading: Image.network(
-              'https://cdn-icons-png.flaticon.com/512/61/61109.png',
-              scale: 0.5,
-              fit: BoxFit.cover,
-              color: Colors.deepOrange,
-            ),
-            title: const Text(
-              'linkedin.com/in/erimedeiros',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 18,
-              ),
-            ),
-          ),
-        ],
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-    );
-
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent.shade700,
       body: Column(
@@ -88,34 +85,63 @@ class _CardAppState extends State<CardApp> {
                 letterSpacing: 1.5),
           ),
           Texts(
-              text: 'DESENVOLVEDOR FLUTTER',
+              text: 'Desenvolvedor Flutter'.toUpperCase(),
               style: TextStyle(
                   fontFamily: 'SourceSansPro',
                   fontSize: 20,
                   color: Colors.deepOrange.shade100,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold)),
+          const SizedBox(height: 15),
           const SizedBox(
-            height: 15,
-          ),
-          Container(
-            height: 1,
             width: 300,
-            color: Colors.deepOrange.shade100,
+            child: Divider(
+              height: 10,
+              color: Colors.white,
+            ),
           ),
           const SizedBox(height: 20),
-          Containers(text: '+55 (99) 98117-3168', icon: Icons.phone_outlined),
-          const SizedBox(height: 20),
-          Containers(text: 'hsymetry@gmail.com', icon: Icons.email_outlined),
-          const SizedBox(height: 20),
+          //Whatsapp
           InkWell(
-            child: Containers(text: 'Sobre', icon: Icons.info_outline),
             onTap: () {
               setState(() {
-                showDialog(context: context, builder: (context) => dialog);
+                _launched = _launchWhatsapp(toWhatsapp!);
               });
             },
-          )
+            child: Containers(text: 'Whatsapp', icon: Icons.whatsapp),
+          ),
+          const SizedBox(height: 20),
+          //Gmail
+          InkWell(
+            onTap: () {
+              setState(() {
+                _launched = _launchInMail(emailLaunchUri);
+              });
+            },
+            child: Containers(text: 'Gmail', icon: Icons.email_outlined),
+          ),
+          const SizedBox(height: 20),
+          //Github
+          InkWell(
+            child:
+                Containers(text: 'Github', icon: Icons.phone_android_outlined),
+            onTap: () {
+              setState(() {
+                _launched = _launchInWeb(toLaunch);
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          //Linkedin
+          InkWell(
+            onTap: () {
+              setState(() {
+                _launched = _launchInWeb(toLaunchWeb);
+              });
+            },
+            child: Containers(
+                text: 'Linkedin', icon: Icons.phone_android_outlined),
+          ),
         ],
       ),
     );
