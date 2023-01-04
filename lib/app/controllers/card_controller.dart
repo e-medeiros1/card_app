0588550c1cff2dfc5b2903de9d20395cbf29ccba
Medeiros <hsymetry@gmail.com>
2023-01-04 13:11:44 -0300

    Revert "Add dotenv & compile for web"
    This reverts commit 1aedabad8db9dfa5e356c1ec83308629600c12f0.

--- lib/app/controllers/card_controller.dart
+++ lib/app/controllers/card_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -6,25 +5,23 @@ class CardController extends GetxController {
   final toWhatsapp = Uri(
     scheme: 'https',
     host: 'wa.me',
-    path: dotenv.env['WHATSAPP'],
+    path: '559981173168',
   ).obs;
 
   final toEmail = Uri(
     scheme: 'mailto',
-    path: dotenv.env['GMAIL'],
+    path: 'hsymetry@gmail.com',
   ).obs;
 
   final toGithub = Uri(
     scheme: 'https',
     host: 'www.github.com',
-    path: dotenv.env['GITHUB'],
+    path: '/e-medeiros1/',
   ).obs;
 
-  final toLinkedin = Uri(
-          scheme: 'https',
-          host: 'www.linkedin.com',
-          path: dotenv.env['LINKEDIN'])
-      .obs;
+  final toLinkedin =
+      Uri(scheme: 'https', host: 'www.linkedin.com', path: '/in/erimedeiros/')
+          .obs;
 
   Future<void> launchWhatsapp(var url) async {
     if (!await launchUrl(
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/card_app.dart';
 
-main() async {
-  await dotenv.load(fileName: '.env');
+main() {
   runApp(
     const CardApp(),
   );
